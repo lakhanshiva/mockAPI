3ff3a46d4e8d8a1970761d00b4279aee87bfd3af
lakhanshiva <lakhanshiva@gmail.com>
2018-03-18 07:56:50 -0600

    tutorial -> mockAPI change

--- env/bin/django-admin.py
+++ env/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/cav/tutorial/env/bin/python2
+#!/home/cav/mockAPI/env/bin/python2
 from django.core import management
 
 if __name__ == "__main__":
--- mockAPI/urls.py
+++ mockAPI/urls.py
@@ -1,4 +1,4 @@
-"""tutorial URL Configuration
+"""mockAPI URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
 from django.conf.urls import url, include
 from rest_framework import routers
-from tutorial.quickstart import views
+from mockAPI.quickstart import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
